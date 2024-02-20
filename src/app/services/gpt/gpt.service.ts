2a826f4bbf68b189e892601e31fc4c9f5e05ed66
Yatin1923 <yatinchokshidsa@gmail.com>
2024-02-20 12:55:10 +0530

    fix

--- src/app/services/gpt/gpt.service.ts
+++ src/app/services/gpt/gpt.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -9,6 +9,8 @@ export class GptService {
   constructor(private http: HttpClient) { }
 
   getGptResponse(message: string){
-    return this.http.get(`http://127.0.0.1:5000/get_gpt_response/${message}`);
+    const headers = new HttpHeaders()
+    .set('Content-Type', 'application/json');
+    return this.http.post(`http://127.0.0.1:5000/get_gpt_response`,{message},{headers});
   }
 }
