6c005448f2f6b6b22ef31b1708b2f2d860e6d729
Yatin1923 <yatinchokshidsa@gmail.com>
2024-02-22 18:20:14 +0530

    copy text

--- src/app/test-case-functions/test-case-functions.component.ts
+++ src/app/test-case-functions/test-case-functions.component.ts
@@ -71,30 +71,31 @@ export class TestCaseFunctionsComponent implements OnInit {
     if(!this.socket){
       this.socket = io("http://127.0.0.1:5000");
       this.socket.on("message",(data:any)=>{
-        debugger
-        let highlighted = false;
         const codeBlockRegex = /```(\w+)\s*([\s\S]*?)```/gs;
         let html = "     <div class='codeBlock'><div class='codeblock-heading'><span>$1</span><button id='copy-button' class='copy-button' clipboard-data='$2'>Copy code</button></div>"
     
-        let sanitizedCode:string = (this.suggestedCode.get(this.suggestFunction) + data.toString()).replace(codeBlockRegex, `${html}<pre id='codeBlock' class=' language-python'><div class='code'><code>$2</code></div></pre></div>`);
+        let sanitizedCode:SafeHtml = (this.suggestedCode.get(this.suggestFunction) + data.toString()).replace(codeBlockRegex, `${html}<pre id='codeBlock' class=' language-python'><div class='code'><code>$2</code></div></pre></div>`);
 
         this.suggestedCode.set(this.suggestFunction,sanitizedCode)
         //  += data.toString(); 
-        if(sanitizedCode.includes("</pre>") && !highlighted){
-          highlighted = true;
+        let value :string = this.sanitizer.sanitize(0,this.suggestedCode.get(this.suggestFunction)??'')??''
+        if(value?.toLowerCase().includes("overandout")){
+
+          let finalcode:string = value?.split('overandout')[0]??''
+          this.suggestedCode.set(this.suggestFunction,this.sanitizer.bypassSecurityTrustHtml(finalcode))
           hljs.highlightAll();
+            document.getElementById('copy-button')?.addEventListener('click',function(){
+                var clipboard = this.getAttribute('clipboard-data')??'';
+                const textarea = document.createElement('textarea');
+                  textarea.value = clipboard;
+                  document.body.appendChild(textarea);
+                  textarea.select();
+                  document.execCommand('copy');
+                  document.body.removeChild(textarea);
+                  this.innerText = 'Copied!';
+                console.log(clipboard);
+        })
         }
-        //  document.getElementById('copy-button')?.addEventListener('click',function(){
-        //     var clipboard = this.getAttribute('clipboard-data')??'';
-        //     const textarea = document.createElement('textarea');
-        //       textarea.value = clipboard;
-        //       document.body.appendChild(textarea);
-        //       textarea.select();
-        //       document.execCommand('copy');
-        //       document.body.removeChild(textarea);
-        //       this.innerText = 'Copied!';
-        //     console.log(clipboard);
-    // })
        
       })
     }
